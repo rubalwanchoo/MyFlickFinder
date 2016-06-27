173c94d123e79fe81c93929d52ee224d08f9554a
rubalwanchoo <rubalwanchoo@gmail.com>
2016-06-26 23:36:02 -0400

    Initial Commit

---
+++ MyFlickFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFlickFinder
+//
+//  Created by Rubal Wanchoo on 6/26/16.
+//  Copyright © 2016 wanchoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
